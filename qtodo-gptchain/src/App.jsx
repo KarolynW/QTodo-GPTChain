b3a66e2d043b3463f23085f9ff90456fc9a6137c
Karolyn Webb <119711647+KarolynW@users.noreply.github.com>
2025-07-21 19:56:27 +0100

    Initialize Vite React app with Tailwind and terminal theme

---
+++ qtodo-gptchain/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="text-center">
+      <pre className="whitespace-pre leading-none">
+{` ____  _____ ____   ___   ___   ___  ____  _____
+|  _ \| ____|  _ \ / _ \ / _ \ / _ \|  _ \| ____|
+| | | |  _| | |_) | | | | | | | | | | | | |  _|
+| |_| | |___|  __/| |_| | |_| | |_| | |_| | |___
+|____/|_____|_|    \___/ \___/ \___/|____/|_____|`}
+      </pre>
+      <div className="blink h-6"></div>
+    </div>
+  );
+}
+
+export default App;
